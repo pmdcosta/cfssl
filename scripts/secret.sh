50d737e801e0217799438d9c9f14da82554fdd78
pmdcosta <pmdcosta@outlook.com>
2016-05-04 16:34:17 +0100

    Debug secret.sh

--- scripts/secret.sh
+++ scripts/secret.sh
@@ -5,7 +5,7 @@
 kubectl create secret generic qold-ssl-ca-cer-dev --from-file=development/ca/ca.pem
 kubectl create secret generic qold-ssl-ca-key-dev --from-file=development/ca/ca-key.pem --from-file=development/ca/ca.pem
 ## RabbitMQ server credentials
-kubectl create secret generic rabbitmq-ssl-server-dev --from-file=development/rabbitmq/server.pem --from-file=development/rabbitmq/server-key.pem
+kubectl create secret generic rabbitmq-ssl-server-dev --from-file=development/server/server.pem --from-file=development/server/server-key.pem
 ## RabbitMQ client credentials
 kubectl create secret generic rabbitmq-ssl-client-dev --from-file=development/cluster/client.pem --from-file=development/cluster/client-key.pem
 ## RabbitMQ gateway credentials
@@ -16,6 +16,6 @@ kubectl create secret generic rabbitmq-ssl-gateway-dev --from-file=development/g
 kubectl create secret generic qold-ssl-ca-cer --from-file=production/ca/ca.pem
 kubectl create secret generic qold-ssl-ca-key --from-file=production/ca/ca-key.pem --from-file=production/ca/ca.pem
 ## RabbitMQ server credentials
-kubectl create secret generic rabbitmq-ssl-server --from-file=production/rabbitmq/server.pem --from-file=production/rabbitmq/server-key.pem
+kubectl create secret generic rabbitmq-ssl-server --from-file=production/server/server.pem --from-file=production/server/server-key.pem
 ## RabbitMQ client credentials
 kubectl create secret generic rabbitmq-ssl-client --from-file=production/cluster/client.pem --from-file=production/cluster/client-key.pem
